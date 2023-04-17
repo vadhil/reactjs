50aff88baa072fad18c7d5043ecc55acc5e6b784
fadhil <88018547+vadhil@users.noreply.github.com>
2023-04-17 17:43:11 +0800

    works, the data we input are array

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
 import './App.css';
 import { useState } from 'react';
-
 function App() {
-      let [todos, setTodos] = useState(['ngoding', 'mengaji', 'belajar']);
-      let [input, setInput] = useState("");
+      let [todoList, setTodoList] = useState([]);
+      let [newTask, setNewTask] = useState("");
       let [isDone, setDone] = useState(false);
 
-
-      const List = () => {
-        return (
-            <div>
-              <ul>
-              {todos.map((todo, i) => { i++;
-                console.log(todo, i);
-               return <li className= {isDone?"stratched": "green"}>{i + ". "}{todo}
-                <button onClick={() => setDone(!isDone)}> Done</button>
-                <button onClick={() => setTodos( todos.filter(todol => todol !== todo) )}> delete </button>
-                </li>
-              })}
-              </ul>
-            </div>
-        );
-        } 
-
-
-  const addTodos = () => {
-    const newTodos = [...todos, input]
-      setTodos(newTodos);
-      setInput("");
-   }
-
- 
-
- 
-
-  const handleInput = (event) => {
-    setInput(event.target.value)
+  const handleChange = (event) => {
+      setNewTask(event.target.value)
   }
-
+  const addTask =()=> {
+    const task = {
+      id: todoList.length === 0? 1 :todoList[todoList.length - 1] + 1,
+      taskName: newTask
+    }
+    setTodoList([...todoList, task]); //keasalahan gw disini karena tidak spesifik menjadikan sbg array
+  }
   return (
     <div className="App">
-    <h1>To Do List</h1>
     <div className="addTask">
-      <button onClick={() => setTodos([...todos, input])}>Add</button>
-      <input type="text" onChange={handleInput} />
+      <h1>To Do List</h1>
+      <button onClick={addTask}>Add task</button>
+      <input type="text" onChange={handleChange} />
     </div>
-    <List/>
+    <div className='list'>
+      
+       { todoList.map((task) => {
+            return(
+            <div>
+            <h1>{task.taskName}</h1>
+            <button > </button>
+            </div>
+            )
+        })}
+      
+    </div>
+  
   </div>
   
   );
@@ -54,6 +43,33 @@ function App() {
 
 
 export default App;
+
+  {/* <div>
+              <ul>
+              {todos.map((todo) => { 
+                
+               return <li className= {todo.isDone?"stratched": "green"}>{todo.id + ". "}{todo.text}
+                <button onClick={() => setDone(!isDone)}> Done</button>
+                <button onClick={() => setTodos( todos.filter(todol => todol.id !== todo.id) )}> delete </button>
+                </li>
+              })}
+              </ul>
+            </div> */}
+
+
+
+  // const addTodos = () => {
+  //   const newTodos = {
+  //     id: todos.length === 0? 1 :todos[todos.length - 1] + 1,
+  //     text: input
+  //   }
+  //     setTodos(newTodos);
+  //     setInput("");
+  //  }
+
+  // const handleInput = (event) => {
+  //   setInput(event.target.value)
+  // }
 
 
 // value={input} 
